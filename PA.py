80c92ebc4b474546669dde12698b2061102d0021
matteokfg <67129306+matteokfg@users.noreply.github.com>
2021-09-19 19:33:57 -0300

    Add docstring e comentários sobre as PA e PG.

--- PA.py
+++ PA.py
@@ -1,27 +1,45 @@
+# importa a biblioteca 'math'
 import math
 
-# cria PA
-#com recursão
+#------------------------------>cria PA:
+#------>com recursão:
 def calcula_progressao(numero, razao):
+    """
+        A função recebe o número inical, a razão, que sempre vai ser somada.
+        A função devolve, inicialmente, o número inicial. Depois, devolve o número mais a
+        razão, e depois, essa soma mais a razão, e assim por diante.
+        A função finaliza quando não tiver mais memória para prosseguir essas somas.
+        Utiliza a recursão.
+    """
     if (razao == 0):
+        # o número permanecerá o mesmo, logo print uma vez e encerra.
         print(numero)
     else:
-        numero = numero + razao
+        # faz a soma do número mais a razão e print.
+        numero += razao
         print(numero)
+        # chama a função ela mesma sendo o número = ele + razão / cria a recursão
         calcula_progressao(numero, razao)
 
-#sem recursão
+#--------->sem recursão
+# define a variável 'infinito' como igual ao número infinito.
 infinito = math.inf
 def cal_pa(numero, razao, limite=infinito):
     """
-        preencher depois
+        A função recebe o número inical, a razão, que sempre vai ser somada e o limite, o qual
+        quando atingido, finaliza a função. Esse limite tem como valor padrão o infinito.
+        A função devolve, inicialmente, o número inicial. Depois, devolve o número mais a
+        razão, e depois, essa soma mais a razão, e assim por diante.
+        Não utiliza a recursão.
     """
+    # print o múmero inicial.
     print(numero)
     while (numero < limite):
         if (razao == 0):
-            # se o número não aumenta, então print uma vez e stop
+            # se o número permanecerá constante, então para a função.
             break
         else:
+            # soma o número com a razão e print.
             numero += razao
             print(numero)
 
--- PG.py
+++ PG.py
@@ -1,24 +1,44 @@
+#importa a biblioteca 'math'
 import math
 
-#cria PG
-#com recursão
+#--------------------------------->cria PG:
+#---------->com recursão:
 def calcula_pg(numero, razao):
+    """
+        A função recebe um número inicial e um, chamado de razão, o qual vai ser multiplicado.
+        A função devolve, inicialmente, o número inicial. Depois devolve o número vezes a razão. E
+        depois, essa multiplicação vezes a razão e assim por diante.
+        Utiliza recursão.
+    """
     if (razao == 1):
+        # como o número permanecerá constante, print uma vez e encerra.
         print(numero)
     else:
-        numero = numero * razao
+        # multiplica o múmero e a razão e print o resultado.
+        numero *= razao
         print(numero)
+        # chama a própria função / cria a recursão.
         calcula_pg(numero, razao)
 
-#sem recursão
+#------------>sem recursão:
+# cria a variável 'infinito' e dá o valor do infinito.
 infinito = math.inf
 def cal_pg(numero, razao, limite=infinito):
+    """
+        A função recebe um número inicial e um, chamado de razão, o qual vai ser multiplicado, e
+        o limite, quando atingido encerra a função, e tem seu valor padrão como infinito.
+        A função devolve, inicialmente, o número inicial. Depois devolve o número vezes a razão. E
+        depois, essa multiplicação vezes a razão e assim por diante.
+        Não utiliza recursão.
+    """
+    # print o número inicial.
     print(numero)
     while (numero <= limite):
         if (razao == 1):
-            # print uma vez e stop
+            # como o número permanece constante, print uma vez e para a função.
             break
         else:
+            # multiplica  o número com a razão e print.
             numero *= razao
             print(numero)
 
