9e028f509bd268f7c2dacc2e10e9c576379550e0
matteokfg <67129306+matteokfg@users.noreply.github.com>
2021-10-11 16:15:57 -0300

    Add the val 'contador'
    Essa variável corresponde ao número do termo.

--- PA.py
+++ PA.py
@@ -3,37 +3,40 @@ import math
 
 #------------------------------>cria PA:
 #------>com recursão:
-def calcula_progressao(numero, razao):
+def calcula_progressao(numero, razao, contador = 0):
     """
         A função recebe o número inical, a razão, que sempre vai ser somada.
         A função devolve, inicialmente, o número inicial. Depois, devolve o número mais a
         razão, e depois, essa soma mais a razão, e assim por diante.
         A função finaliza quando não tiver mais memória para prosseguir essas somas.
+        A variável 'contador' demonstra o número do termo correspondente a variável 'número'.
         Utiliza a recursão.
     """
     if (razao == 0):
         # o número permanecerá o mesmo, logo print uma vez e encerra.
-        print(numero)
+        print(f"A{contador} : {numero}")
     else:
         # faz a soma do número mais a razão e print.
         numero += razao
-        print(numero)
+        contador += 1
+        print(f"A{contador} : {numero}")
         # chama a função ela mesma sendo o número = ele + razão / cria a recursão
-        calcula_progressao(numero, razao)
+        calcula_progressao(numero, razao, contador)
 
 #--------->sem recursão
 # define a variável 'infinito' como igual ao número infinito.
 infinito = math.inf
-def cal_pa(numero, razao, limite=infinito):
+def cal_pa(numero, razao, limite=infinito, contador = 1):
     """
         A função recebe o número inical, a razão, que sempre vai ser somada e o limite, o qual
         quando atingido, finaliza a função. Esse limite tem como valor padrão o infinito.
         A função devolve, inicialmente, o número inicial. Depois, devolve o número mais a
         razão, e depois, essa soma mais a razão, e assim por diante.
+        A variável 'contador' representa o número do termo correspondente a variável 'número'.
         Não utiliza a recursão.
     """
     # print o múmero inicial.
-    print(numero)
+    print(f"A{contador} : {numero}")
     while (numero < limite):
         if (razao == 0):
             # se o número permanecerá constante, então para a função.
@@ -41,8 +44,10 @@ def cal_pa(numero, razao, limite=infinito):
         else:
             # soma o número com a razão e print.
             numero += razao
-            print(numero)
+            contador += 1
+            print(f"A{contador} : {numero}")
 
 
 if __name__ == '__main__':
+    print(calcula_progresao(1,1))
     print(cal_pa(1,1))
