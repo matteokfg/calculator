cb35cf4f60f7424b33e229d1c6eb1112354d0ab3
matteokfg <67129306+matteokfg@users.noreply.github.com>
2022-01-10 17:03:46 -0300

    Update calculator.exs
    Organized the docs in module and function.

--- calculator.exs
+++ calculator.exs
@@ -14,29 +14,42 @@ defmodule Calculadora do
     end
   end
 
-  def multiplicaçao(num1, num2) do
-    #função da multipicação
+  def multiplicacao(num1, num2) do
+    @doc """
+    -> Função da multipicação.
+
+    ## Teste:
+      Calculadora.multiplicacao(13, 13)
+      iex> 13 vezes 13 e igual a: 169
+    """
+
     IO.puts("#{num1} vezes #{num2} e igual a: #{num1*num2}")
   end
 
   def divisao(num1, num2) do
-    #função da divisão, que devolve tanto a divisão, quanto o resto da divisão
+    @doc """
+    -> Função da divisão, que devolve tanto a divisão, quanto o resto da divisão.
+
+    ## Teste:
+      Calculadora.divisao(60, 10)
+      iex> 60 dividido por 10 da: 6.0, com resto: 0
+    """
+
     IO.puts(
       "#{num1} dividido por #{num2} da: #{div(num1, num2)}, com resto: #{rem(num1, num2)}"
       )
   end
 
   def potenciacao(num1, num2) do
-    #função da potencição
-    # essa forma de fazer potencia nao aguenta potencias muito grandes
+    @doc """
+    -> Função da potencição, essa forma de fazer potencia não aguenta potencias muito grandes.
+
+    ## Teste:
+      Calculadora.potenciacao(4, 2)
+      iex> 4 elevado a 2 da: 16
+    """
+
     num3 = :math.pow(num1, num2)
     IO.puts("#{num1} elevado a #{num2} da: #{num3}")
   end
 end
-
-#Exemplos de testes:
-# Calculadora.adiçao(1, 1)
-# Calculadora.subtraçao(1, 2)
-# Calculadora.multiplicaçao(13, 13)
-# Calculadora.divisao(60, 10)
-# Calculadora.potenciacao(4, 2)
