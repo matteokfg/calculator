9f17bb989181ae5eefe7f0ee1322318afcc33e03
Matteo K. F. Giuliani <67129306+matteokfg@users.noreply.github.com>
2022-11-01 10:19:09 -0300

    Debugged the issue of compile with the docs
    doc can't be inside ou fuctions/macros, need to be with the moduledoc

--- calculator.exs
+++ calculator.exs
@@ -4,30 +4,55 @@ defmodule Calculadora do
   `subtracao/2`, `multiplicacao/2`, `divisao/2`, `potenciacao/2` e `radiciacao/1`.
   """
 
+  @doc """
+  Função da adição.
+  ## Teste:
+    Calculadora.adicao(1, 1)
+    iex> 2
+  """
+
+  @doc """
+  -> Função da subtração, que vai até zero.
+  ## Teste:
+    Calculadora.subtraçao(1, 2)
+    iex> 1 - 2 e negativo!
+    Calculadora.subtracao(8, 3)
+    iex> 8 - 3 e igual a: 5
+  """
+
+  @doc """
+  -> Função da multipicação.
+  ## Teste:
+    Calculadora.multiplicacao(13, 13)
+    iex> 13 vezes 13 e igual a: 169
+  """
+
+  @doc """
+  -> Função da divisão, que devolve tanto a divisão, quanto o resto da divisão.
+  ## Teste:
+    Calculadora.divisao(60, 10)
+    iex> 60 dividido por 10 da: 6.0, com resto: 0
+  """
+
+  @doc """
+  -> Função da potencição, essa forma de fazer potencia não aguenta potencias muito grandes.
+  ## Teste:
+    Calculadora.potenciacao(4, 2)
+    iex> 4 elevado a 2 da: 16.0
+  """
+
+  @doc """
+  -> Essa forma faz raíz quadrada.
+  ## Teste:
+    Calculadora.radiciacao(16)
+    iex> A raiz quadrada de 16 e: 4.0
+  """
+
   def adicao(num1, num2) do
-    @doc """
-    Função da adição.
-
-    ## Teste:
-      Calculadora.adicao(1, 1)
-      iex> 2
-    """
-
     IO.puts("#{num1} + #{num2} e igual a: #{num1 + num2}")
   end
 
   def subtracao(num1, num2) do
-    @doc """
-    -> Função da subtração, que vai até zero.
-
-    ## Teste:
-      Calculadora.subtraçao(1, 2)
-      iex> 1 - 2 e negativo!
-
-      Calculadora.subtracao(8, 3)
-      iex> 8 - 3 e igual a: 5
-    """
-
     if num1 - num2 < 0 do
       IO.puts("#{num1} - #{num2} e negativo!")
     else
@@ -36,52 +61,23 @@ defmodule Calculadora do
   end
 
   def multiplicacao(num1, num2) do
-    @doc """
-    -> Função da multipicação.
-
-    ## Teste:
-      Calculadora.multiplicacao(13, 13)
-      iex> 13 vezes 13 e igual a: 169
-    """
-
-    IO.puts("#{num1} vezes #{num2} e igual a: #{num1*num2}")
+    multi = num1*num2
+    IO.puts("#{num1} vezes #{num2} e igual a: #{multi}")
   end
 
   def divisao(num1, num2) do
-    @doc """
-    -> Função da divisão, que devolve tanto a divisão, quanto o resto da divisão.
-
-    ## Teste:
-      Calculadora.divisao(60, 10)
-      iex> 60 dividido por 10 da: 6.0, com resto: 0
-    """
-
+    divi = div(num1, num2)
     IO.puts(
-      "#{num1} dividido por #{num2} da: #{div(num1, num2)}, com resto: #{rem(num1, num2)}"
+      "#{num1} dividido por #{num2} da: #{divi}, com resto: #{rem(num1, num2)}"
       )
   end
 
   def potenciacao(num1, num2) do
-    @doc """
-    -> Função da potencição, essa forma de fazer potencia não aguenta potencias muito grandes.
-
-    ## Teste:
-      Calculadora.potenciacao(4, 2)
-      iex> 4 elevado a 2 da: 16.0
-    """
-
     num3 = :math.pow(num1, num2)
     IO.puts("#{num1} elevado a #{num2} da: #{num3}")
   end
 
   def radiciacao(num1) do
-    @doc """
-    -> Essa forma faz raíz quadrada.
-    ## Teste:
-      Calculadora.radiciacao(16)
-      iex> A raiz quadrada de 16 e: 4.0
-    """
-
     num2 = :math.sqrt(num1)
     IO.puts("A raiz quadrada de #{num1} e: #{num2}")
   end
@@ -92,38 +88,60 @@ defmodule Teorema do
     Esse modulo contem teormeas matematicos, que poderao ser resolvidos com ele.
   """
 
-  def de_pitagoras_hip(b, c) do
-    @doc """
-      -> calculo da hipotenusa ( maior lado do triangulo ), a partir dos dois catetos
-      ## Teste:
-        Teorema.de_pitagoras_hip(3, 4)
-        iex>A hipotenusa e: 5.0
-    """
+  @doc """
+  -> calculo da hipotenusa ( maior lado do triangulo ), a partir dos dois catetos
+  ## Teste:
+    Teorema.de_pitagoras_hip(3, 4)
+    iex>A hipotenusa e: 5.0
+  """
 
+  @doc """
+  -> calculo o cateto, a partir da hipotenusa e do outro cateto
+  ## Teste:
+    Teorema.de_pitagoras_cat(5, 3)
+    iex>O cateto e: 4.0
+  """
+
+  @doc """
+  -> calcula as duas raizes de uma equacao de segundo grau usando a formula de bhaskara
+  ## Teste:
+    Teorema.de_bhaskara(1, 8, -9)
+    iex>Aplicando a formula, o primeiro x e: 1 e o segundo x e: -9
+  """
+
+  @doc """
+  -> calcula o motante final e depois o juros usando o capital inicial, a taxa e o tempo percorrido,
+  sendo esses ultimos na mesma medida de tempo
+  ## Teste:
+    Teorema.de_juros_compostos(1000, 0.1, 10)
+    iex>Aplicando a formula, o motante final e: 1010.04 , sendo o juros: 10.04
+  """
+
+  @doc """
+  -> calcula o parametro que for nil, ou seja o q nao foi calculado ainda
+  ## Teste:
+    Teorema.fundamental_da_reta(nil, -1.5, -2, -2.25, 0)
+    iex>Y e igual a 3
+  """
+
+  @doc """
+  -> calcula a area de um triangulo a partir de sua base e altura
+  ## Teste:
+    Teorema.area_do_triangulo(4,5)
+    iex>O triangulo de base 4 e altura 5 tem area de 10 u².
+  """
+
+  def de_pitagoras_hip(b, c) do
     a = :math.sqrt(:math.pow(b, 2) + :math.pow(c, 2))
     IO.puts("A hipotenusa e: #{a}")
   end
 
   def de_pitagoras_cat(a, c) do
-    @doc """
-      -> calculo o cateto, a partir da hipotenusa e do outro cateto
-      ## Teste:
-        Teorema.de_pitagoras_cat(5, 3)
-        iex>O cateto e: 4.0
-    """
-
     b = :math.sqrt(:math.pow(a, 2) - :math.pow(c, 2))
     IO.puts("O cateto e: #{b}")
   end
 
   def de_bhaskara(a, b, c) do
-    @doc """
-      -> calcula as duas raizes de uma equacao de segundo grau usando a formula de bhaskara
-      ## Teste:
-      Teorema.de_bhaskara(1, 8, -9)
-      iex>Aplicando a formula, o primeiro x e: 1 e o segundo x e: -9
-    """
-
     delta = :math.pow(b, 2) - 4*a*c
     x1 = div(trunc(-1*b + :math.sqrt(delta)), 2*a)
     x2 = div(trunc(-1*b - :math.sqrt(delta)), 2*a)
@@ -131,27 +149,12 @@ defmodule Teorema do
   end
 
   def de_juros_compostos(c, i, n) do
-    @doc """
-      -> calcula o motante final e depois o juros usando o capital inicial, a taxa e o tempo percorrido,
-      sendo esses ultimos na mesma medida de tempo
-      ## Teste:
-      Teorema.de_juros_compostos(1000, 0.1, 10)
-      iex>Aplicando a formula, o motante final e: 1010.04 , sendo o juros: 10.04
-    """
-
     m = c * :math.pow((1 + i), n)
     j = m - c
     IO.puts("Aplicando a formula, o motante final e: #{m} , sendo o juros: #{j}")
   end
 
   def fundamental_da_reta(y \\ nil, yo \\ nil, m \\ nil, x \\ nil, xo \\ nil) do
-    @doc """
-      -> calcula o parametro que for nil, ou seja o q nao foi calculado ainda
-      ## Teste:
-      Teorema.fundamental_da_reta(nil, -1.5, -2, -2.25, 0)
-      iex>Y e igual a 3
-    """
-
     cond do
       y == nil and yo != nil and m != nil and x != nil and xo != nil ->
         y = m * (x - xo) + yo
@@ -176,4 +179,17 @@ defmodule Teorema do
       true -> IO.puts("Esta faltando informaçoes, tente novamente")
     end
   end
+
+  def area_do_triangulo(b \\ nil, h \\ nil) do
+    area = div(b * h, 2)
+    IO.puts("O triangulo de base #{b} e altura #{h} tem area de #{area} u².")
+  end
 end
+
+#Exemplos de testes:
+# Calculadora.adiçao(1, 1)
+# Calculadora.subtraçao(1, 2)
+# Calculadora.multiplicaçao(13, 13)
+# Calculadora.divisao(60, 10)
+# Calculadora.potenciacao(4, 2)
+# Teorema.area_do_triangulo(4,5)
